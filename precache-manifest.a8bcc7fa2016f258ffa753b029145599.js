e4e7692ef5e89aabb496f68026fb158db57ec767
devendradeore <ddeore568@gmail.com>
2025-01-06 14:00:03 +0530

    Updates

---
+++ precache-manifest.a8bcc7fa2016f258ffa753b029145599.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "919f74a12e312064a55fc04896f72ab8",
+    "url": "/real-estate-management/index.html"
+  },
+  {
+    "revision": "a32374fa6f394a7289ae",
+    "url": "/real-estate-management/static/css/2.dd939bbb.chunk.css"
+  },
+  {
+    "revision": "cb7ecdc4850b21584fc8",
+    "url": "/real-estate-management/static/css/main.6457199f.chunk.css"
+  },
+  {
+    "revision": "a32374fa6f394a7289ae",
+    "url": "/real-estate-management/static/js/2.06380ffe.chunk.js"
+  },
+  {
+    "revision": "5c308587bbb541708688",
+    "url": "/real-estate-management/static/js/3.6fc501d6.chunk.js"
+  },
+  {
+    "revision": "cb7ecdc4850b21584fc8",
+    "url": "/real-estate-management/static/js/main.905a8764.chunk.js"
+  },
+  {
+    "revision": "0534a23af6dff2495577",
+    "url": "/real-estate-management/static/js/runtime~main.0f1d050a.js"
+  },
+  {
+    "revision": "6119d1bbf58989bd7ef04af59e89da1c",
+    "url": "/real-estate-management/static/media/about.6119d1bb.jpg"
+  },
+  {
+    "revision": "955db14048d099d893ab5c13df344126",
+    "url": "/real-estate-management/static/media/bg-image.955db140.jpg"
+  },
+  {
+    "revision": "0c361df5b2e34713aa867d20dea55a5a",
+    "url": "/real-estate-management/static/media/carousel3.0c361df5.jpg"
+  },
+  {
+    "revision": "d7800041b6d4caf5a9e3d59c8c738bdf",
+    "url": "/real-estate-management/static/media/e.d7800041.mp4"
+  },
+  {
+    "revision": "0ab3921d9b80975c5597432ab59f5d0a",
+    "url": "/real-estate-management/static/media/fa-brands-400.0ab3921d.ttf"
+  },
+  {
+    "revision": "8b0ddedbb27cbc9971c8667caa8a0cc1",
+    "url": "/real-estate-management/static/media/fa-brands-400.8b0ddedb.woff2"
+  },
+  {
+    "revision": "20206738b2bffb741b00200d5d3d6d20",
+    "url": "/real-estate-management/static/media/fa-regular-400.20206738.ttf"
+  },
+  {
+    "revision": "61f30b79daf5b31f0d254a31fba66158",
+    "url": "/real-estate-management/static/media/fa-regular-400.61f30b79.woff2"
+  },
+  {
+    "revision": "c64278386c2bbb5e293e11b94ca2f6d1",
+    "url": "/real-estate-management/static/media/fa-solid-900.c6427838.woff2"
+  },
+  {
+    "revision": "e2ceb83946c9e5fc7eab24453a03bffb",
+    "url": "/real-estate-management/static/media/fa-solid-900.e2ceb839.ttf"
+  },
+  {
+    "revision": "4bc58bc16bb05a05d3a47a4f7e143b75",
+    "url": "/real-estate-management/static/media/fa-v4compatibility.4bc58bc1.woff2"
+  },
+  {
+    "revision": "d20cedd7e254d4b58b721b6995ca52b4",
+    "url": "/real-estate-management/static/media/fa-v4compatibility.d20cedd7.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/real-estate-management/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/real-estate-management/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/real-estate-management/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/real-estate-management/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/real-estate-management/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "28a3caaee25c4c3c9f0711a3169a0296",
+    "url": "/real-estate-management/static/media/house1.28a3caae.jpg"
+  },
+  {
+    "revision": "f1c4aa82ef169144a70ee9b19a7fc4c6",
+    "url": "/real-estate-management/static/media/room1.f1c4aa82.jpg"
+  }
+]);
